9f4163118aa3f414e0980cfa88e9859f84992fa3
reyfinanta <reyfinanta2@gmail.com>
2021-10-31 17:28:30 +0700

    menambahkan file sistem

---
+++ sistem/lingkaran.py
@@ -0,0 +1,17 @@
+#profile saya
+print ("program menghitung luas lingkaran dan keliling lingkarang \n")
+print ("nama Muhammad Reynal Finanta\n NIm:312110441")
+print ("tanggal lahir 3 juni 2001\n")
+
+#luas lingkaran
+phi = 3.14
+r = float (input ("masukan panjang jari jari lingkaran : "))
+luas_lingkaran = phi*r*r
+
+print ("luas lingkaran adalah :",luas_lingkaran)
+
+#keliling lingkaran
+r = float (input ( "masukan jari jari lingkaran :"))
+keliling_lingkaran = phi*r
+
+print ("keliling lingkaran adalah :" , keliling_lingkaran)
